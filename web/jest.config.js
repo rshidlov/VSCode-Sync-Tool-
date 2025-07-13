11cfbeb194021c70f19748d0ff3efcaa2a7497ca
Roi Shidlovsky <shidlo@apple.com>
2025-07-13 13:31:34 +0300

    Remove Babel configuration and update Jest to use ts-jest for TypeScript support; clean up package dependencies by removing unused Babel presets and jest-related packages.

--- web/jest.config.js
+++ web/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleDirectories: ['node_modules', '<rootDir>'],
